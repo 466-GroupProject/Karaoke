45c79e093e5157cf06d3d0f7b77e1f0838470490
Michael Rasmussen <z1696323@turing.cs.niu.edu>
2023-04-30 07:53:26 -0500

    Fixed tables to be sortable

--- DJInterface.php
+++ DJInterface.php
@@ -3,8 +3,6 @@
 <h1 style="text-align: center">Welcome DJ</h1>
 <h2 style="text-align: center">Choose a song to play next</h2>
 
-<h1 style ='text-align: right;'> Priotity Queue <h1>
-<h1 style ='text-align: left;' > Free Queue <h1>
 
 <?php
     include("library.php");
@@ -22,7 +20,7 @@
             
         //label and draw table
         echo "<h1> Free Queue </h1>" ;	
-        draw_table($rows);
+        drawTable($rows);
     
         //run querry
         $rs = $pdo->query("Select Name, StageName, Song.SongID, Title, Artist, SignUpTime, Cost FROM Song, Users, SignUp WHERE Song.SongID = SignUp.SongID AND SignUp.UsersID = Users.UsersID AND QueueType = 'P' ORDER BY Cost DESC;");
@@ -30,7 +28,7 @@
             
         //label and draw table
         echo "<h1> Priority Queue </h1>";
-        draw_table($rows);
+        drawTable($rows);
     } 
     catch (PDOException $e) {
         die("<p>Query failed: {$e->getMessage()}</p>\n");
