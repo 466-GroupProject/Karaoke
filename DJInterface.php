4c3408df9f50d559f8eb6618cf33d8f00b13ea06
Michael Rasmussen <z1696323@turing.cs.niu.edu>
2023-05-01 13:41:45 -0500

    Added the FileID and updated buttons

--- DJInterface.php
+++ DJInterface.php
@@ -7,7 +7,7 @@
 </div>
 
 <h1 style="text-align: center">Welcome DJ</h1>
-<h2 style="text-align: center">Choose a song to play next</h2>
+<h2 style="text-align: center">Choose a File to play next</h2>
 
 
 <?php
@@ -24,7 +24,7 @@
 
 
         // set SQL For free queue
-        $sql = "SELECT Name, StageName, Song.SongID, Title, Artist, SignUpTime 
+        $sql = "SELECT FileID, Name, StageName, Title, Artist, SignUpTime
                 FROM Song, Users, SignUp 
                 WHERE Song.SongID = SignUp.SongID 
                 AND SignUp.UsersID = Users.UsersID 
@@ -36,12 +36,16 @@
         $statement->execute();
         $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+        // Set FileID to play on button press        
+        $File = (empty($rows)) ? "None Available" : $rows[0]['FileID'];
+
+
         // label Table
         echo "<h1> Free Queue </h1>" ;
 
         // Button to remove top entry of the table
         echo "<form action=\"DJInterface.php\" method=\"POST\">";
-        echo "<input type=\"submit\" name=\"Free\" value=\"Play Next Free Queue Song\"/>";
+        echo "<input type=\"submit\" name=\"Free\" value=\"Play File $File\"/>";
         echo "</form>";
 
         if (empty($rows))
@@ -78,7 +82,7 @@
         if (isset($_POST['Cost']))
         {
             // set SQL statement
-            $sql = "SELECT Name, StageName, Song.SongID, Title, Artist, SignUpTime, Cost    
+            $sql = "SELECT FileID, Name, StageName, Title, Artist, SignUpTime, Cost    
                     FROM Song, Users, SignUp 
                     WHERE Song.SongID = SignUp.SongID 
                     AND SignUp.UsersID = Users.UsersID 
@@ -93,7 +97,7 @@
         else if (isset($_POST['Time']))
         {
             // Set SQL statement
-            $sql = "SELECT Name, StageName, Song.SongID, Title, Artist, SignUpTime, Cost    
+            $sql = "SELECT FileID, Name, StageName, Title, Artist, SignUpTime, Cost    
                     FROM Song, Users, SignUp 
                     WHERE Song.SongID = SignUp.SongID 
                     AND SignUp.UsersID = Users.UsersID 
@@ -107,8 +111,9 @@
         // if no button was pressed sort by Cost by default
         else
         {
+            echo "YAAAAAa";
             // set SQL statement
-            $sql = "SELECT Name, StageName, Song.SongID, Title, Artist, SignUpTime, Cost    
+            $sql = "SELECT FileID, Name, StageName, Title, Artist, SignUpTime, Cost    
                     FROM Song, Users, SignUp 
                     WHERE Song.SongID = SignUp.SongID 
                     AND SignUp.UsersID = Users.UsersID 
@@ -118,6 +123,14 @@
             // set Sorted
             $Sorted = "Cost";
         }
+
+        // prepare, execute, and fetch Priority Table
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        // Set FileID to play on button press  
+        $File = (empty($rows)) ? "None Available" : $rows[0]['FileID'];
             
         // label table
         echo "<h1> Priority Queue </h1>";
@@ -126,42 +139,10 @@
         // button to remove top entry from table
         echo "<form action=\"DJInterface.php\" method=\"POST\">";
         echo "<input type=\"hidden\" name=\"Hidden\" value = $Sorted />";
-        echo "<input type=\"submit\" name=\"Priority\" value=\"Play Next Priority Queue Song\"/>";
+        echo "<input type=\"submit\" name=\"Priority\" value=\"Play File $File\"/>";
         echo "</form>";
 
-        // if button is pressed and sorted by cost
-        if(isset($_POST['Priority']) && $_POST['Hidden'] == "Cost") 
-        {
-            // set SQL statement
-            $sqlPlay = "DELETE FROM SignUp 
-                        WHERE QueueType = 'P' 
-                        ORDER BY Cost DESC LIMIT 1;";
-            
-            // Prepare, execute, and fetch
-            $statement = $pdo->prepare($sqlPlay);
-            $statement->execute();
-            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-            // refresh to show results
-            echo '<meta http-equiv="refresh" content="0">';
-        }
-
-        // if button is pressed and sorted by Time
-        else if(isset($_POST['Priority']) && $_POST['Hidden'] == "Time") 
-        {
-            // Set SQL statement
-            $sqlPlay = "DELETE FROM SignUp 
-                        WHERE QueueType = 'P' 
-                        ORDER BY SignUpTime ASC LIMIT 1;";
-            
-            // Prepare, execute, and fetch
-            $statement = $pdo->prepare($sqlPlay);
-            $statement->execute();
-            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
-            
-            // refresh to show results
-            echo '<meta http-equiv="refresh" content="0">';
-        }
 
         //Sort Button Cost
         echo "<form action=\"DJInterface.php\" method=\"POST\">";
@@ -173,19 +154,54 @@
         echo "<input type=\"submit\" name=\"Time\" value=\"Sort By Sign Up Time\"/>";
         echo "</form>";
 
-        // prepare, execute, and fetch Priority Table
-        $statement = $pdo->prepare($sql);
-        $statement->execute();
-        $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+                // prepare, execute, and fetch Priority Table
+                $statement = $pdo->prepare($sql);
+                $statement->execute();
+                $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
 
         if (empty($rows))
         {
-            echo "<h3> There is currently no one signed up to sing in the free queue. </h3>";
+            echo "<h3> There is currently no one signed up to sing in the priority queue. </h3>";
         }
         else
         {
             // Print priority table
             draw_table($rows);
+        }
+
+        // if button is pressed and sorted by cost
+        if(isset($_POST['Priority']) && $_POST['Hidden'] == "Cost") 
+        {
+            // set SQL statement
+            $sqlPlay = "DELETE FROM SignUp 
+                        WHERE QueueType = 'P' 
+                        ORDER BY Cost DESC LIMIT 1;";
+                    
+            // Prepare, execute, and fetch
+            $statement = $pdo->prepare($sqlPlay);
+            $statement->execute();
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+            // refresh to show results
+            echo '<meta http-equiv="refresh" content="0">';
+        }
+        
+        // if button is pressed and sorted by Time
+        else if(isset($_POST['Priority']) && $_POST['Hidden'] == "Time") 
+        {
+            // Set SQL statement
+            $sqlPlay = "DELETE FROM SignUp 
+                        WHERE QueueType = 'P' 
+                        ORDER BY SignUpTime ASC LIMIT 1;";
+                    
+            // Prepare, execute, and fetch
+            $statement = $pdo->prepare($sqlPlay);
+            $statement->execute();
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+                    
+            // refresh to show results
+            echo '<meta http-equiv="refresh" content="0">';
         }
     } 
     catch (PDOException $e) {
